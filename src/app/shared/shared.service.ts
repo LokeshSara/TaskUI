92a632e65da5ed26c66e900c6456eb9ca9a7f1c7
=LSaravananH <=lokesh_saravanan@hotmail.com>
2018-12-28 02:47:53 -0600

    Validatins

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -15,17 +15,17 @@ export class ApiService {
 
 
   //private weatherUrl = 'https://query.yahooapis.com/v1/public/yql?format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&q=';
-    private taskapiUrl = 'http://localhost:54913/api/task';
-    private userapiUrl = 'http://localhost:54913/api/user';
-    private projectapiUrl = 'http://localhost:54913/api/project';
+    // private taskapiUrl = 'http://localhost:54913/api/task';
+    // private userapiUrl = 'http://localhost:54913/api/user';
+    // private projectapiUrl = 'http://localhost:54913/api/project';
 
     // private taskapiUrl = 'http://suchi-pc/TaskApi/api/task';
     //  private userapiUrl = 'http://suchi-pc/TaskApi/api/user';
     //   private projectapiUrl = 'http://suchi-pc/TaskApi/api/project';
 
-      // private taskapiUrl = 'http://localhost/TaskApi/api/task';
-      // private userapiUrl = 'http://localhost/TaskApi/api/user';
-      //  private projectapiUrl = 'http://localhost/TaskApi/api/project';
+      private taskapiUrl = 'http://localhost/TaskApi/api/task';
+      private userapiUrl = 'http://localhost/TaskApi/api/user';
+       private projectapiUrl = 'http://localhost/TaskApi/api/project';
 
     /**
      *
